e10f6644aaa06488ce9064bc88b1532e5e5748b6
Soren Hein <soren.hein@gmail.com>
2022-05-14 13:29:27 +0200

    Working on Covers.h

--- src/distributions/covers/Covers.cpp
+++ src/distributions/covers/Covers.cpp
@@ -147,6 +147,168 @@ const Cover& Covers::lookup(const Cover& cover) const
 }
 
 
+
+/////////////////////////////////////////////
+
+// TODO
+extern Edata edata;
+
+template<class C, class T>
+void Covers::explainTemplate(
+  const Tricks& tricks,
+  const unsigned char tmin,
+  const unsigned numStrategyTops,
+  const C& candidates,
+  CoverStack<T>& stack,
+  CoverTableau& solution)
+{
+  stack.emplace(tricks, tmin, candidates.begin());
+
+edata.stackActual = 0;
+edata.firstFix = 0;
+edata.stackMax = 0;
+edata.numSteps = 0;
+edata.numCompares = 0;
+edata.numSolutions = 0;
+edata.numBranches = 0;
+// cout << edata.strHeader();
+
+  while (! stack.empty())
+  {
+    auto handle = stack.extract(stack.begin());
+    StackEntry<T>& stackElem = handle.value();
+// Can probably avoid overwriting in CoverTableau, but for now
+// we make a copy
+StackEntry<T> stackElemCopy = stackElem;
+
+    CoverTableau& tableau = stackElem.tableau;
+    auto candIter = stackElem.iter;
+
+size_t tmp = stack.size();
+unsigned tmpSolutions = edata.numSolutions;
+bool branchFlag = false;
+unsigned branchLimit;
+if (tmp < 10000)
+  branchLimit = 5;
+else if (tmp < 30000)
+  branchLimit = 3;
+else if (tmp < 50000)
+  branchLimit = 2;
+else
+  branchLimit = 1;
+
+    while (candIter != candidates.end())
+    {
+      if (candIter->effectiveDepth() > numStrategyTops)
+      {
+        candIter++;
+        continue;
+      }
+
+      const unsigned char headroom = tableau.headroom(solution);
+
+      if (candIter->minComplexityAdder(tableau.getResidualWeight()) > 
+          headroom)
+      {
+        // As the covers are ordered, later covers have no chance either.
+        break;
+      }
+
+      if (candIter->getComplexity() > headroom)
+      {
+        // The current cover may be too complex, but there may be others.
+        candIter++;
+        continue;
+      }
+
+      // if (stackIter->tableau.attempt(cases, candIter, stack, solution))
+      if (tableau.attempt(cases, candIter, stack, solution))
+      {
+        // We found a solution.  It may have replaced the previous one.
+if (edata.firstFix == 0)
+  edata.firstFix = edata.numSteps;
+        break;
+      }
+
+      candIter++;
+
+      if (candIter != candidates.end() && stack.size() - tmp > branchLimit)
+      {
+        branchFlag = true;
+        break;
+      }
+    }
+
+edata.numBranches += stack.size() - tmp;
+
+if (stack.size() > edata.stackMax)
+  edata.stackMax = stack.size();
+
+if (edata.numSolutions > tmpSolutions)
+{
+// unsigned cs = stack.size();
+  stack.prune(solution);
+// cout << "Erased " << cs - stack.size() << " << elements\n";
+
+
+}
+
+edata.stackActual = stack.size();
+
+edata.numSteps++;
+if (edata.numSteps % 100 == 0)
+{
+  T t;
+  // cout << edata.str(t.ID());
+}
+
+    if (branchFlag)
+    {
+      // cout << "Branch\n";
+      stackElemCopy.iter = candIter;
+      const unsigned w = stackElemCopy.tableau.getResidualWeight();
+      if (w == 0)
+      {
+        assert(false);
+      }
+      const unsigned char minCompAdder = 
+        candIter->minComplexityAdder(w);
+      stackElemCopy.tableau.project(minCompAdder);
+      stack.insert(stackElemCopy);
+    }
+
+
+  }
+
+/* */
+T t;
+cout << edata.strHeader();
+cout << edata.str(t.ID());
+/* */
+
+}
+
+template void Covers::explainTemplate<CoverStore, Cover>(
+  const Tricks& tricks,
+  const unsigned char tmin,
+  const unsigned numStrategyTops,
+  const CoverStore& candidates,
+  CoverStack<Cover>& stack,
+  CoverTableau& solution);
+
+template void Covers::explainTemplate<RowStore, CoverRow>(
+  const Tricks& tricks,
+  const unsigned char tmin,
+  const unsigned numStrategyTops,
+  const RowStore& candidates,
+  CoverStack<CoverRow>& stack,
+  CoverTableau& solution);
+
+/////////////////////////////////////////////
+
+
+
+
 void Covers::explain(
   const list<Result>& results,
   const unsigned numStrategyTops,
